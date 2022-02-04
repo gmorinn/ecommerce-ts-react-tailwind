1aeccd70661236d8ead5d6911c4cd68fb264687d
Guillaume MORIN <guillaume.morin@epitech.eu>
2022-02-04 11:25:39 +0400

    env

--- src/components/firebase/firebase.ts
+++ src/components/firebase/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/database'
 import 'firebase/compat/storage'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBbSfDw1SnV4Nd9h5m-jwDMAgDoPPPzB0k",
-  authDomain: "starterpack-85abc.firebaseapp.com",
-  databaseURL: "https://starterpack-85abc-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "starterpack-85abc",
-  storageBucket: "starterpack-85abc.appspot.com",
-  messagingSenderId: "354625368863",
-  appId: "1:354625368863:web:318752b113f868c63e762a"
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_API_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_API_DATABASEURL,
+  projectId: process.env.REACT_APP_API_PROJECTID,
+  storageBucket: process.env.REACT_APP_API_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_API_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_API_APPID
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
